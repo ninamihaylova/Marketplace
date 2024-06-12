9e253868e22ca225f304bab9924cfeed96877d5e
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-12 21:27:54 +0300

    Initial commit

---
+++ Marketplace/Program.cs
@@ -0,0 +1,25 @@
+﻿class Marketplace {
+    public static void Main(String[] args)
+    {
+        string product=Console.ReadLine();
+        string day=Console.ReadLine();
+        if (product=="Banana")
+        {
+            if (day == "Weekday")
+                Console.WriteLine($"{2.50:F2}");
+            else
+                Console.WriteLine($"{2.70:F2}");
+        }
+        else if (product=="Apple"){
+            if(day=="Weekday") 
+                Console.WriteLine($"{1.30:F2}");
+            else 
+                Console.WriteLine("${1.60:F2}");
+        }
+        else if (product=="Kiwi") {
+            if(day=="Weekday") 
+                Console.WriteLine($"{2.20:F2}");
+            else
+                Console.WriteLine($"{3.00:F2}"); }
+    }
+    }
